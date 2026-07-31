01ef1ba5654e7f30dfe0f1ce76c0da6f7effd442
Muktha A R <muktha.akathuttel@gmail.com>
2026-07-31 18:10:50 +0530

    Add Gmail email thread and message models

--- apps/api/app/models/__init__.py
+++ apps/api/app/models/__init__.py
@@ -1,13 +1,23 @@
 from app.models.customer import Customer
+from app.models.email_message import EmailMessage
+from app.models.email_thread import EmailThread
+from app.models.gmail_connection import GmailConnection
 from app.models.organization import Organization
 from app.models.product import Product
+from app.models.quotation import Quotation
+from app.models.quotation_item import QuotationItem
 from app.models.user import User, UserRole
 
 
 __all__ = [
     "Customer",
+    "EmailMessage",
+    "EmailThread",
+    "GmailConnection",
     "Organization",
     "Product",
+    "Quotation",
+    "QuotationItem",
     "User",
     "UserRole",
 ]
